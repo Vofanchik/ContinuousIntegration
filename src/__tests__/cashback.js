d4f69994cfa07ec5f5ee636690be5db4e372fd57
Vladimir Kalinin <Vofamchic@mail.ru>
2024-01-02 18:59:35 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/__tests__/cashback.js 	modified:   src/__tests__/test.js 	modified:   src/calculate.js 	modified:   src/cashback.js

--- src/__tests__/cashback.js
+++ src/__tests__/cashback.js
@@ -1,21 +1,20 @@
-import calculateCashback from '../cashback'
+import calculateCashback from '../cashback';
 
 test('cash at 500', () => {
-    const result = calculateCashback(500);
-    expect(result).toBe(0)
-})
+  const result = calculateCashback(500);
+  expect(result).toBe(0);
+});
 
 const dataList = [
-    ['silver', 10000, 200],
-    ['gold', 100000, 5000],
-    ['no', 500, 0],
+  ['silver', 10000, 200],
+  ['gold', 100000, 5000],
+  ['no', 500, 0],
 
-]
+];
 
-
-const handler = test.each(dataList)
+const handler = test.each(dataList);
 
 handler('testing cashback %s and %i', (status, amount, expected) => {
-    const result = calculateCashback(amount)
-    expect(result).toBe(expected)
-})
+  const result = calculateCashback(amount);
+  expect(result).toBe(expected);
+});
--- src/calculate.js
+++ src/calculate.js
@@ -1,17 +1,15 @@
-export function calculateTotal(purchases, applyDiscount) {
-    const sum = purchases.reduce((total, item)=>{
-        return total + (item.count * item.price)
-    }, 0)
+export default function calculateTotal(purchases, applyDiscount) {
+  const sum = purchases.reduce((total, item) => total + (item.count * item.price), 0);
 
-    if (applyDiscount){
-        return sum * 0.75
-    }
-    return sum
-    // let result = 0;
-    // for (let index = 0; index < purchases.length; index++) {
-    //     result += purchases[index].count * purchases[index].price;      
-    // }
-    // return result;
+  if (applyDiscount) {
+    return sum * 0.75;
+  }
+  return sum;
+  // let result = 0;
+  // for (let index = 0; index < purchases.length; index++) {
+  //     result += purchases[index].count * purchases[index].price;
+  // }
+  // return result;
 }
 
 // module.exports = {
